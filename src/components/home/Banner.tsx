f0e7821c37ac2be5803098254253ce3550c4640a
freakeinstein <wow.dr.dre@gmail.com>
2022-04-07 14:14:11 +0530

    fixed explore link + footer copyright

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -12,7 +12,7 @@ const Banner = () => {
         <p className="text-center text-gray-900 text-lg font-bold mb-5">Liberate those powerful bookmarks you are keeping under the mattress 🤷</p>
         <p className="text-center text-gray-700">Aquila Network allows the entire world to discover your favorite blogs and newsletters.<br/> Spread new ideas, show what you are standing for. Works with paywall-protected websites.</p>
         <div className="text-center mt-10">
-          <a href="https://x.aquila.network/explore.html" target="_blank" className="bg-white p-2 mr-4  text-blue-600 underline" rel="noreferrer">Explore</a>
+          <a href="https://x.aquila.network/explore" target="_blank" className="bg-white p-2 mr-4  text-blue-600 underline" rel="noreferrer">Explore</a>
           <a href="https://airtable.com/shrhvlKgL6otYCnlb" target="__blank" className="bg-red-500 p-2 rounded-full px-4 text-white">Get Started</a>
         </div>
         </div>
