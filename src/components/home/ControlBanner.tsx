f038754ad5fe75f0975189f8743575e39a4bf43a
freakeinstein <wow.dr.dre@gmail.com>
2022-06-01 15:41:03 +0530

    content changes

--- src/components/home/ControlBanner.tsx
+++ src/components/home/ControlBanner.tsx
@@ -13,7 +13,7 @@ const ControlBanner = () => {
 		<div className="w-9/12 md:w-2/4 h-full items-center flex mt-8 md:mt-0">
 			<div>
 			<h3 className="text-2xl mb-5 font-semibold font-raleway ">You’ve got all the control.</h3>
-			<p className="text-gray-800 font-light text-xl mb-5">You're ready to go once you've installed a browser extension. <br/>We're not interested in your personal information. Source code is available for public scrutiny.</p>
+			<p className="text-gray-800 font-light text-xl mb-5">You're ready to go once you've installed a browser extension. <br/>We're not interested in your personal information. That's why we made our software Open Source for public scrutiny.</p>
 			{/* <p className="text-gray-800 font-light text-xl mb-3">Your login data strictly remains on your browser. You can install the AquilaX addon and provide it with your secret key. </p> */}
 			{/* <p className="text-gray-800 font-light text-xl">Skeptical about a pre-built addon? You are welcome to download our open source code, verify it and build addon yourself. </p> */}
 			</div>
